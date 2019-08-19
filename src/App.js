39df64aa8d10506281546950e422cddf9a167744
vrborjasm <vrborjasm@gmail.com>
2019-08-19 17:10:54 -0400

    feat: include Todo Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
+import Header from './components/Header'
+
 class App extends Component {
   state={
-
+    todolist: []
   }
   render() {
     return (
       <div>
-        
+        <Header title="Todo List"/>  
       </div>
     );
   }
