bae694abc8b65e093b7a2d5edeadf3114081a967
vrborjasm <vrborjasm@gmail.com>
2019-12-02 14:48:52 -0300

    add status to app.js state and checkstatus funtionality

--- src/App.js
+++ src/App.js
@@ -1,148 +1,225 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from './components/Header'
-import Admin from './components/Admin'
-import TodoList from './components/TodoList';
-import NewTodo from './components/NewTodo';
-import api from './api';
-
+import Header from "./components/Header";
+import Admin from "./components/Admin";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import api from "./api";
 
 class App extends Component {
-  state={
+  state = {
     todoList: [],
-    sortBy: 'creation',
-    filterBy: 'all'
-  }
+    sortBy: "creation",
+    filterBy: "all"
+  };
 
-  componentDidMount () {
-    api.getList()
+  componentDidMount() {
+    api
+      .getList()
       .then(response => this.setState({ todoList: response.data }))
-      .catch(err => alert('No nos hemos podido comunicar con el servidor, intentalo nuevamente'))
-    }
-
-    handleChangeSort= (e) => {
-      this.setState({ sortBy: e.target.value })  
+      .catch(err =>
+        alert(
+          "No nos hemos podido comunicar con el servidor, intentalo nuevamente"
+        )
+      );
   }
 
-    handleChangeFilter= (e) => {
-      this.setState({ filterBy: e.target.value })  
-  }
-
-    handleCheck = (oldTodo) => {
-    const todoList = this.state.todoList.filter(todo => todo.id !== oldTodo.id)
-    todoList.push({ ...oldTodo, selected: !oldTodo.selected })
-    this.setState({ todoList })
-  }
-
-  modifyDate = (e,oldTodo) => {
-    const todoList = this.state.todoList.filter(todo => todo.id !== oldTodo.id)
-    this.setState({ todoList })
-    api.patch(oldTodo.id, { date: e.target.value })
-    .then(response => {
-      this.setState({ todoList: this.includeInTodoList(response.data) });
+  time = setInterval(() => {
+    this.state.todoList.forEach(todo => {
+      const todoList = this.state.todoList.filter(compareTodo => compareTodo.id !== todo.id);
+      this.setState({ todoList });
+      const newStatus = this.checkStatus(todo.date, todo.status)
+      if (todo.status !== newStatus) {
+        api
+        .patch(todo.id, {
+          status: newStatus
+        })
+        .then(response => {
+          this.setState({ todoList: this.includeInTodoList(response.data) });
+        })
+        .catch(err => {
+          this.setState({ todoList: this.includeInTodoList({ ...todo }) });
+          alert(
+            "Hubo un problema conectando al servidor. Intentalo de nuevo mas tarde!"
+          );
+        });
+      } else {
+        todoList.push(todo);
+        this.setState({ todoList });
+      }
     })
-    .catch(err => {
-      this.setState({ todoList: this.includeInTodoList({ ...oldTodo }) });
-      alert('Hubo un problema conectando al servidor. Intentalo de nuevo mas tarde!');
-    })
-    
-  }
+  }, 30000);
 
-  includeInTodoList = (todo) => {
+  handleChangeSort = e => {
+    this.setState({ sortBy: e.target.value });
+  };
+
+  handleChangeFilter = e => {
+    this.setState({ filterBy: e.target.value });
+  };
+
+  handleCheck = oldTodo => {
+    const todoList = this.state.todoList.filter(todo => todo.id !== oldTodo.id);
+    todoList.push({ ...oldTodo, selected: !oldTodo.selected });
+    this.setState({ todoList });
+  };
+
+  modifyDate = (e, oldTodo) => {
+    const todoList = this.state.todoList.filter(todo => todo.id !== oldTodo.id);
+    this.setState({ todoList });
+    api
+      .patch(oldTodo.id, {
+        date: e.target.value,
+        status: this.checkStatus(e.target.value, oldTodo.status)
+      })
+      .then(response => {
+        this.setState({ todoList: this.includeInTodoList(response.data) });
+      })
+      .catch(err => {
+        this.setState({ todoList: this.includeInTodoList({ ...oldTodo }) });
+        alert(
+          "Hubo un problema conectando al servidor. Intentalo de nuevo mas tarde!"
+        );
+      });
+  };
+
+  includeInTodoList = todo => {
     const todoList = this.state.todoList.map(todo => ({ ...todo }));
     todoList.push(todo);
 
     return todoList;
-  }
+  };
 
   freeTodos = () => {
     const todoList = [];
 
     this.state.todoList.forEach(todo => {
       if (todo.selected) {
-        api.patch(todo.id, { finished: true })
+        api
+          .patch(todo.id, { finished: true, status: "done" })
           .then(response => {
             this.setState({ todoList: this.includeInTodoList(response.data) });
           })
           .catch(err => {
             this.setState({ todoList: this.includeInTodoList({ ...todo }) });
-            alert('Hubo un problema conectando al servidor. Intentalo de nuevo mas tarde!');
-          })
+            alert(
+              "Hubo un problema conectando al servidor. Intentalo de nuevo mas tarde!"
+            );
+          });
       } else {
         todoList.push({ ...todo });
       }
-    })
+    });
 
     this.setState({ todoList });
-  }
+  };
 
-  addTodo = (todo) => {
-    api.post(todo).then(response => {
-      this.setState({ todoList: this.includeInTodoList(response.data) });
-    })
-    .catch(err => {
-      alert('Hubo un problema conectando al servidor. Intentalo de nuevo mas tarde!');
-    })
-}
+  checkStatus = (date, status) => {
+    if (status !== "done") {
+      if (status !== "alarm" && new Date(date) > Date.now()) {
+        return "alarm";
+      } else if (status !== "warning" && new Date(date) < Date.now()) {
+        return "warning";
+      }
+    }
+    return status;
+  };
 
-    deleteTodo = (id) => {
-      api.delete(id).then(responde => {
-        const todoList = this.state.todoList.filter(todo => todo.id !== id);
-        this.setState({ todoList }); 
+  addTodo = todo => {
+    api
+      .post({ ...todo, status: this.checkStatus(todo.date, todo.status) })
+      .then(response => {
+        this.setState({ todoList: this.includeInTodoList(response.data) });
       })
       .catch(err => {
-        alert('Hubo un problema conectando con el servidor. Intentelo de nuevo mas tarde');
+        alert(
+          "Hubo un problema conectando al servidor. Intentalo de nuevo mas tarde!"
+        );
+      });
+  };
+
+  deleteTodo = id => {
+    api
+      .delete(id)
+      .then(responde => {
+        const todoList = this.state.todoList.filter(todo => todo.id !== id);
+        this.setState({ todoList });
       })
+      .catch(err => {
+        alert(
+          "Hubo un problema conectando con el servidor. Intentelo de nuevo mas tarde"
+        );
+      });
+  };
+
+  sortedList = todoList => {
+    const order = this.state.sortBy;
+    let todof = [];
+    let todol = [];
+    let todop = [];
+
+    if (order === "creation") {
+      todoList = todoList.sort((todoA, todoB) => todoA.id < todoB.id);
+    } else if (order === "expiration") {
+      todoList = todoList.sort(
+        (todoA, todoB) => new Date(todoA.date) > new Date(todoB.date)
+      );
+    } else {
+      todoList.forEach(todo => {
+        if (todo.finished) {
+          todof.push(todo);
+        } else if (new Date(todo.date) > new Date()) {
+          todop.push(todo);
+        } else {
+          todol.push(todo);
+        }
+      });
+
+      todoList = todol.concat(todop, todof);
+    }
+    return todoList;
+  };
+
+  filteredList = () => {
+    const filter = this.state.filterBy;
+    let todoList = [];
+
+    if (filter === "all") {
+      todoList = this.state.todoList;
+    } else if (filter === "free") {
+      todoList = this.state.todoList.filter(todo => todo.finished);
+    } else if (filter === "pending") {
+      todoList = this.state.todoList.filter(
+        todo => !todo.finished && new Date(todo.date) > new Date()
+      );
+    } else {
+      todoList = this.state.todoList.filter(
+        todo => !todo.finished && new Date(todo.date) < new Date()
+      );
     }
 
-    sortedList = (todoList) => {
-      const order = this.state.sortBy
-      let todof = []
-      let todol = []
-      let todop = []
-      
-      if (order === "creation") {
-        todoList = todoList.sort((todoA, todoB) => todoA.id < todoB.id);
-      } else if (order === "expiration") {
-        todoList = todoList.sort((todoA, todoB) => new Date(todoA.date) > new Date(todoB.date));
-      } else {
-        todoList.forEach(todo => {
-          if (todo.finished) { todof.push(todo) }
-          else if (new Date(todo.date) > new Date()) { todop.push(todo) }
-          else {todol.push(todo)}
-        });
-
-        todoList = todol.concat(todop,todof)
-      }
-      return todoList
-    }  
-
-    filteredList = () => {
-      const filter = this.state.filterBy
-      let todoList = []
-      
-      if (filter === "all") {
-        todoList = this.state.todoList
-      } else if (filter === "free") {
-        todoList = this.state.todoList.filter(todo => todo.finished)
-      } else if (filter === "pending") {
-        todoList = this.state.todoList.filter(todo => !todo.finished && new Date(todo.date) > new Date())
-      } else {
-        todoList = this.state.todoList.filter(todo => !todo.finished && new Date(todo.date) < new Date())
-      }
-
-      return todoList
-    }  
+    return todoList;
+  };
 
   render() {
-    let todoList = this.filteredList()
-    todoList = this.sortedList(todoList)
+    let todoList = this.filteredList();
+    todoList = this.sortedList(todoList);
     return (
       <div>
-        <Header title="todoList"/>
-        <Admin freeTodos={this.freeTodos} handleChangeSort={this.handleChangeSort} handleChangeFilter={this.handleChangeFilter}/>
-        <TodoList todoList={todoList} handleCheck={this.handleCheck} modifyDate={this.modifyDate} deleteTodo={this.deleteTodo}/>
-        <NewTodo addTodo={this.addTodo}/> 
+        <Header title="todoList" />
+        <Admin
+          freeTodos={this.freeTodos}
+          handleChangeSort={this.handleChangeSort}
+          handleChangeFilter={this.handleChangeFilter}
+        />
+        <TodoList
+          todoList={todoList}
+          handleCheck={this.handleCheck}
+          modifyDate={this.modifyDate}
+          deleteTodo={this.deleteTodo}
+          changeStatus={this.changeStatus}
+        />
+        <NewTodo addTodo={this.addTodo} />
       </div>
     );
   }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,59 +1,53 @@
-import React from 'react';
+import React from "react";
 
-import './Todo.css';
+import "./Todo.css";
 
-const changeStatus = ({ finished, date }) => {
-  if (finished) return 'done'
-    else if (new Date(date) > Date.now()) return 'alarm'
-    return 'warning'
-  }
-
-const Todo = ({ todo, handleCheck, modifyDate, deleteTodo }) => {  
-  const status = changeStatus(todo)
+const Todo = ({ todo, handleCheck, modifyDate, deleteTodo }) => {
   const extraClasses = {
-    'done': 'todo__status--finished',
-    'alarm': 'todo__status--pending',
-    'warning': 'todo__status--late',
+    done: "todo__status--finished",
+    alarm: "todo__status--pending",
+    warning: "todo__status--late"
   };
 
-  const classStatus = `card my-3 ${extraClasses[status]}`
+  const classStatus = `card my-3 ${extraClasses[todo.status]}`;
   return (
-      <div className={classStatus}>
-          <div className="card-body px-1 py-2">
-              <div className="row">
-                  <div className=" col-1 text-center my-auto">
-                  <i className="material-icons" onClick={() => handleCheck(todo)}>{ todo.selected ? "check_box" : "check_box_outline_blank"}</i>
-                  </div>         
-                  <div className="col-11 col-md-5 col-lg-6 my-auto">
-                      <p className="mb-0">{todo.description}</p>
-                  </div>          
-                  <div className="col-12 col-md-5 col-lg-4 my-auto text-center">
-                     
-                      <div className="form-group mb-0">
-                          <div>
-                            
-                          <input 
-                          onChange={(e) => modifyDate(e,todo)}
-                          type="date" 
-                          className="form-control date_input_todo d-inline-block py-0" 
-                          name={`date-${todo.id}`}
-                          value={todo.date}
-                          />
-                          
-                          <span><i class="material-icons">calendar_today</i></span>
-                          
-                          </div>
-                      </div>
-                  </div>
-                  <div className="col-12 col-md-1 my-auto text-center">
-                  <i className="material-icons">{status}</i>
-                  </div>
-              </div>  
+    <div className={classStatus}>
+      <div className="card-body px-1 py-2">
+        <div className="row">
+          <div className=" col-1 text-center my-auto">
+            <i className="material-icons" onClick={() => handleCheck(todo)}>
+              {todo.selected ? "check_box" : "check_box_outline_blank"}
+            </i>
           </div>
-          <i className="material-icons" onClick={() => deleteTodo(todo.id)}>check_box</i>
-
+          <div className="col-11 col-md-5 col-lg-6 my-auto">
+            <p className="mb-0">{todo.description}</p>
+          </div>
+          <div className="col-12 col-md-5 col-lg-4 my-auto text-center">
+            <div className="form-group mb-0">
+              <div>
+                <input
+                  onChange={e => modifyDate(e, todo)}
+                  type="date"
+                  className="form-control date_input_todo d-inline-block py-0"
+                  name={`date-${todo.id}`}
+                  value={todo.date}
+                />
+                <span>
+                  <i class="material-icons">calendar_today</i>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-1 my-auto text-center">
+            <i className="material-icons">{todo.status}</i>
+          </div>
+        </div>
+      </div>
+      <i className="material-icons" onClick={() => deleteTodo(todo.id)}>
+        check_box
+      </i>
     </div>
-  )
+  );
 };
 
 export default Todo;
